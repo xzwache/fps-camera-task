6c28114f72154f8ddd6c6ce4aeae03584828aa90
Egor <garemexpo@gmail.com>
2020-10-14 17:56:39 +0300

    Crosshair with render texture implementation

---
+++ FPS-camera-task/Assets/Scripts/Crosshair.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour {
+    
+    [SerializeField] private Camera _camera;
+    [SerializeField] private MeshRenderer _crosshairRenderer;
+    private RenderTexture _renderTexture;
+
+    private void Start() {
+        _renderTexture = new RenderTexture(512,512, 16);
+
+        _camera.targetTexture = _renderTexture;
+
+        _crosshairRenderer.material.mainTexture = _renderTexture;
+    }
+}
